d79462142a520b111c4b73f07be3962661a65ce4
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2021-03-20 17:01:17 -0400

    feat: db_connection.php added

--- db_connection.php
+++ db_connection.php
@@ -3,5 +3,11 @@
 $server_name = 'localhost';
 $username = 'root';
 $password = '';
+$db = 'crud';
 
-$conn = new PDO("mysql:host=$server_name;dbname=php-crud", $username, $password);
+try {
+    $conn = new PDO("mysql:host=$server_name;$db", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed";
+}
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php 
+
+    require 'db_connection.php';
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     
@@ -12,42 +18,46 @@
     
     <body>
 
-        <h1>PHP CRUD (Another bored to-do list)</h1>
-
-        <section class="add">
+        <main>
         
-            <form action="" method="post">
+            <h1>PHP CRUD (Another bored to-do list)</h1>
+
+            <section class="add">
             
-                <label for="title">Title:</label>            
-                <input type="text" name="title" placeholder="Todo title" required>
+                <form action="" method="post">
+                
+                    <label for="title">Title:</label>            
+                    <input type="text" name="title" placeholder="Todo title" required>
 
-                <label for="description">Description:</label>
-                <input type="text" name="description" placeholder="Todo description" required>
+                    <label for="description">Description:</label>
+                    <input type="text" name="description" placeholder="Todo description" required>
 
-                <label for="urgency">Urgency:</label>
-                <select>
-                    <option value="low">Low</option>
-                    <option value="medium">Medium</option>
-                    <option value="high">High</option>
-                </select>
+                    <label for="urgency">Urgency:</label>
+                    <select>
+                        <option value="low">Low</option>
+                        <option value="medium">Medium</option>
+                        <option value="high">High</option>
+                    </select>
 
-                <button type="submit">Add todo</button>
+                    <button type="submit">Add todo</button>
 
-            </form>
+                </form>
+            
+            </section>
+
+            <section class="todos">
+                
+                <div class="todo">
+                
+                    <h2></h2>
+                    <p></p>
+                    <small></small>
+                
+                </div>
+            
+            </section>
         
-        </section>
-
-        <section class="todos">
-            
-            <div class="todo">
-            
-                <h2></h2>
-                <p></p>
-                <small></small>
-            
-            </div>
-        
-        </section>
+        </main>
 
     </body>
 
